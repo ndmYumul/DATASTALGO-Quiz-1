a5703f36be5968047480e69439eab4243b032e6d
unknown <nmyumul@student.hau.edu.ph>
2025-12-03 23:08:50 +0800

    updated App.js

--- chongs/src/App.js
+++ chongs/src/App.js
@@ -3,19 +3,23 @@ import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductView from './screens/ProductView';
 
 function App() {
   return (
     <Router>
+      <div>
       <Header />
       <main className="py-3">
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/" element={<ProductView />} exact />
           </Routes>
         </Container>
       </main>
       <Footer />
+      </div>
     </Router>
   );
 }
--- chongs/src/components/Header.jsx
+++ chongs/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
